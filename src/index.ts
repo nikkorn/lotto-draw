3685439a60074ccd77a507159b2fffc8f47f6e22
Nikolas Howard <nikolas.howard@LighthouseSystems.com>
2022-04-25 13:17:04 +0100

    Added participants argument for createLotto

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
 import { Lotto } from "./Lotto";
 
-export default function createLotto<TParticipant = any>(): Lotto<TParticipant> {
+/**
+ * A type defining a participant and ticket count.
+ */
+export type ParticipantEntry<TParticipant> = [TParticipant, number];
+
+export function createLotto<TParticipant = any>(participants?: ParticipantEntry<TParticipant>[]): Lotto<TParticipant> {
+    // Create the Lotto instace.
     const lotto = new Lotto();
 
+    // If the lotto participants have been defined upfront then we will need to add them all to our lotto instance now.
+    if (participants) {
+        participants.forEach(([participant, tokens]) => lotto.add(participant, tokens));
+    }
+
+    // Return the Lotto instance.
     return lotto;
 } 
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,8 +1,14 @@
-import createLotto from "../src/index";
+import { createLotto } from "../src/index";
 
 describe('does a', () => {
     test('thing', () => {
-        createLotto<number>().add(3, 3).draw()
-        expect(createLotto().draw()).toBe(15);
+        // Getting wrong type here! Add should take/return its own participant type (TAddedParticipant)?
+        createLotto().add(3, 3).add(9, 3).draw();
+
+        // Draw return value typed as any as no initial participants added.
+        createLotto().draw();
+
+        // Draw return value typed as number as no number participants added.
+        createLotto([[6, 4], [5, 4]]).draw();
     });
 });
